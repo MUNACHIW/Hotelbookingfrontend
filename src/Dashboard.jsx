feecdc6b35c1bde8a2d8f8e61ecd9fd9d60c5008
MUNACHIW <munasom25@gmail.com>
2025-03-04 10:18:10 +0100

    final state

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -9,18 +9,21 @@ import './Dashboard.css';
 const Dashboard = () => {
   const [bookings, setBookings] = useState([]);
   const [editingBooking, setEditingBooking] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Check if bookings data exists in local storage
     const cachedBookings = localStorage.getItem('bookings');
     if (cachedBookings) {
       setBookings(JSON.parse(cachedBookings));
+      setLoading(false);
     } else {
       fetchBookings();
     }
   }, []);
 
   const fetchBookings = async () => {
+    setLoading(true); // Set loading to true when fetching data
     try {
       const response = await axios.get('https://backendbookingsystem-3.onrender.com/bookings');
       setBookings(response.data.bookings);
@@ -28,6 +31,8 @@ const Dashboard = () => {
       localStorage.setItem('bookings', JSON.stringify(response.data.bookings));
     } catch (error) {
       console.error('Error fetching bookings:', error);
+    } finally {
+      setLoading(false); // Set loading to false when data fetching is complete
     }
   };
 
@@ -65,15 +70,21 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <h1 className="dashboard-title">Admin Dashboard</h1>
-      <div className="chart-container">
-        <BookingChart bookings={bookings} />
-      </div>
-      <div className="form-container">
-        <BookingForm onCreateOrUpdate={handleCreateOrUpdateBooking} editingBooking={editingBooking} />
-      </div>
-      <div className="list-container">
-        <BookingList bookings={bookings} onEdit={handleEditBooking} onDelete={handleDeleteBooking} />
-      </div>
+      {loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <>
+          <div className="chart-container">
+            <BookingChart bookings={bookings} />
+          </div>
+          <div className="form-container">
+            <BookingForm onCreateOrUpdate={handleCreateOrUpdateBooking} editingBooking={editingBooking} />
+          </div>
+          <div className="list-container">
+            <BookingList bookings={bookings} onEdit={handleEditBooking} onDelete={handleDeleteBooking} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
